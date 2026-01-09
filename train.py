ac288a5fa03b6e331a1dccbdf6af1a2a0aff26ca
2022BCS0007-albertpaul <alberts@hotmail.com>
2026-01-09 12:22:04 +0530

    Model-LinearRegression, test_size-0.2, preprocessing-standard

--- train.py
+++ train.py
@@ -8,14 +8,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-
-
 DATA_PATH = "dataset/winequality.csv"
 data = pd.read_csv(DATA_PATH, sep=';')
 
-
 target_column = "quality"
-
 X = data.drop(columns=[target_column])
 y = data[target_column]
 
@@ -29,27 +25,20 @@ scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-
+# Model
 model = LinearRegression()
 model.fit(X_train_scaled, y_train)
-
 
 y_pred = model.predict(X_test_scaled)
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-
+# Save model and results
 joblib.dump(model, "output/model.pkl")
-
-metrics = {
-    "mean_squared_error": mse,
-    "r2_score": r2
-}
-
+metrics = {"mean_squared_error": mse, "r2_score": r2}
 with open("output/results.json", "w") as f:
     json.dump(metrics, f, indent=4)
-
 
 print("Model Evaluation Metrics")
 print("------------------------")
